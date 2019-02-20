99748c66cc78f7b9db003951f35d63a8b847f5f2
Igor Mironov <mcs6502-sek@yahoo.com.au>
2019-02-21 08:27:01 +1100

    add more tests for block parsing

--- print_views.py
+++ print_views.py
@@ -15,26 +15,54 @@ __copyright__ = "Copyright 2019, Igor Mironov"
 __license__ = "Apache v2.0"
 
 
-def build_block(bits):
-    """Parse a string of 0/1 bits into a three-dimensional list of booleans"""
-    n = len(bits)  # the number of bits in the bitmap
-    # dimensions of the block -- for now assume it is a cube
-    a = b = c = int(round(n ** (1. / 3)))
-    return np.asarray([c == '1' for c in bits]).reshape([a, b, c])
+def take_n(n, s):
+    """Take n characters from string s"""
+    m = len(s)  # the actual number of bits
+    if m == n:
+        return s
+    a, b = divmod(n, m)  # repeat s 'a' times and add 'b' chars to get size n
+    return s * a + s[:b]
+
+
+def build_block(bits, dims):
+    """Parse a string of 0/1 bits into a n-dimensional list of booleans"""
+    m = int(np.prod(dims))  # the number of bits required to cover dims
+    s = take_n(m, bits)  # gets the string of the right size
+    a = np.asarray([c != '0' for c in s])
+    return a.reshape(dims)
 
 
 def print_bitmap_views(block, views):
     print(block)
 
 
-def print_views(files, views):
-    # a bitmap is a string of '0' and '1' characters ending with a 'b'
-    bitmap_re = re.compile('([01]+)b')
-    for line in fileinput.input(files):
-        for bitmap_match in bitmap_re.finditer(line):
+class BlockBuilder(object):
+
+    def __init__(self, dims=None):
+        self.dims = dims
+        # a bitmap is a string of '0' and '1' characters ending with a 'b'
+        self.bitmap_re = re.compile('([01]+)b')
+
+    def block_dims(self, bits):
+        if self.dims is not None:
+            return self.dims
+        n = len(bits)  # the number of bits in the bitmap
+        # dimensions of the block -- for now assume it is a cube
+        m = int(round(n ** (1. / 3)))
+        return [m, m, m]
+
+    def parse_blocks(self, line):
+        for bitmap_match in self.bitmap_re.finditer(line):
             # the group as defined in re drops the 'b' keeping just the bits
             bits = bitmap_match.group(1)
-            block = build_block(bits)
+            dims = self.block_dims(bits)
+            yield build_block(bits, dims)
+
+
+def print_views(builder, files, views):
+    for line in fileinput.input(files):
+        blocks = builder.parse_blocks(line)
+        for block in blocks:
             print_bitmap_views(block, views)
 
 
@@ -49,4 +77,4 @@ if __name__ == '__main__':
     parser.add_argument('files', nargs='*', metavar='FILE',
                         help='input files to read (if empty then read stdin)')
     args = parser.parse_args()
-    print_views(args.files, args.views)
+    print_views(BlockBuilder(), args.files, args.views)
--- test_print_views.py
+++ test_print_views.py
@@ -2,20 +2,65 @@ import unittest
 
 import numpy as np
 
-from print_views import build_block
+from print_views import BlockBuilder, build_block
 
 __author__ = "Igor Mironov"
 __copyright__ = "Copyright 2019, Igor Mironov"
 __license__ = "Apache v2.0"
 
+T = True
 
-class BlockTest(unittest.TestCase):
-    def test_build_block(self):
-        np.testing.assert_array_equal([
+F = False
+
+
+def assert_array_equal(a, b):
+    np.testing.assert_array_equal(a, b)
+
+
+class BlockBuilderTest(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super(BlockBuilderTest, self).__init__(*args, **kwargs)
+
+        self.input_3 = '000001010011100101110111000b'
+        self.array_3 = [
             [[False, False, False], [False, False, True], [False, True, False]],
             [[False, True, True], [True, False, False], [True, False, True]],
             [[True, True, False], [True, True, True], [False, False, False]]
-        ], build_block('000001010011100101110111000'))
+        ]
+
+        self.input_2 = '00011011b'
+        self.array_2 = [
+            [[False, False], [False, True]],
+            [[True, False], [True, True]]
+        ]
+
+    def test_build_block(self):
+        bits = self.input_3[:-1]
+        dims = [3, 3, 3]
+        assert_array_equal(self.array_3, build_block(bits, dims))
+
+    def test_parse_cube(self):
+        builder = BlockBuilder()
+        blocks = builder.parse_blocks(f"({self.input_3}; {self.input_2})")
+        assert_array_equal(self.array_3, next(blocks))
+        assert_array_equal(self.array_2, next(blocks))
+
+    def test_parse_rect(self):
+        builder = BlockBuilder([2, 4])
+        blocks = builder.parse_blocks(f"{self.input_2} {self.input_3}")
+        array_a = [[F, F, F, T], [T, F, T, T]]
+        assert_array_equal(array_a, next(blocks))
+        array_b = [[F, F, F, F], [F, T, F, T]]  # truncated input_3
+        assert_array_equal(array_b, next(blocks))
+
+    @staticmethod
+    def test_parse_short_input():
+        builder = BlockBuilder([2, 3, 4])
+        blocks = builder.parse_blocks('0010101b')  # will replicate input
+        a = [[[F, F, T, F], [T, F, T, F], [F, T, F, T]],
+             [[F, T, F, F], [T, F, T, F], [T, F, F, T]]]
+        assert_array_equal(a, next(blocks))
 
 
 if __name__ == '__main__':
